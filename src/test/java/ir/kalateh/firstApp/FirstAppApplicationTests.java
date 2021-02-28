0fa7b81aee17e4dc0c176cb02fd5874fae5b56c0
hossein <h.kalateh72@gmail.com>
2021-03-01 02:27:49 +0330

    add pom and update git ignore

---
+++ src/test/java/ir/kalateh/firstApp/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.kalateh.firstApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
